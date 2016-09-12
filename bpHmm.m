85560aa0eb8c7b6ed44d4bed313d597f944c7d87
Mo Chen <sth4nth@gmail.com>
2016-09-12 19:47:16 +0800

    add unormalized bp for hmm

--- bpHmm.m
+++ bpHmm.m
@@ -1,12 +1,14 @@
 function [ gamma, llh ] = bpHmm( x, s, A, E )
 % Belief propagation alogrithm for HMM to compute posterior p(z_i|x)
+%   The algorithm maintain two set of messages, forward \Alpha(z) (downward \pi(z) from parents) and
+%   backward \Beta(z) (upward \lambda(z) from children). 
 % Input:
 %   x: 1xn observation
 %   s: kx1 starting probability of p(z_1|s)
 %   A: kxk transition probability
 %   E: kxd emission probability
 % Output:
-%   m: 1xn posterier p(z_i|x)
+%   gamma: 1xn posterier p(z_i|x)
 %   llh: loglikelihood or evidence lnp(x)
 % written by Mo Chen sth4nth@gmail.com
     n = size(x,2);
@@ -14,17 +16,19 @@ function [ gamma, llh ] = bpHmm( x, s, A, E )
     X = sparse(x,1:n,1,d,n);
     M = full(E*X);
     
+    Alpha = zeros(k,n);
+    Beta = zeros(k,n);
+    Alpha(:,1) = s;
+    Beta(:,n) = 1;
     At = A';
-    c = zeros(1,n); % normalization constant
-    gamma = zeros(k,n);
-    [gamma(:,1),c(1)] = nml(s.*M(:,1),1);
     for i = 2:n
-        [gamma(:,i),c(i)] = nml((At*gamma(:,i-1)).*M(:,i),1);  % 13.59
+        Alpha(:,i) = At*(Alpha(:,i-1).*M(:,i-1));
     end
-   
     for i = n-1:-1:1
-        gamma(:,i) = nml(bsxfun(@times,A,gamma(:,i)),1)*gamma(:,i+1);
+        Beta(:,i) = A*(Beta(:,i+1).*M(:,i+1));
     end
-    llh = sum(log(c));
+    [gamma, c] = nml(Alpha.*Beta.*M,1);
+%     llh = sum(log(c));
+    llh = c;
 end
 
