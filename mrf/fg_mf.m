1788d950f0e8c8b8aba7ccec8d95a3695b1b30f1
Mo Chen <sth4nth@gmail.com>
2017-02-15 18:02:27 +0800

    update

--- mrf/fg_mf.m
+++ mrf/fg_mf.m
@@ -1,1 +1,45 @@
 function [nodeBel, edgeBel, L] = fg_mf(nodePot, edgePot, B)
+% Mean field for factor graph
+B = logical(B);
+tol = 1e-4;
+epoch = 50;
+L = -inf(1,epoch+1);
+[nodeBel,lnZ] = softmax(nodePot,1);    % init nodeBel
+for t = 1:epoch
+    for i = 1:size(B,2)
+        e = B(:,i);                         % edge indcies in the neighborhood
+        n = B(e,:);                         % node indecis in the neighborhood
+        n(:,i) = false;                     % exclude self
+        
+        [ej,nj] = fgn(B,i);
+        ep = edgePot(:,:,ej);    % edgePot in neighborhood
+        nb = nodeBel(:,nj);      % nodeBel in neighborhood
+%         m = zeros(size(nb));    % incoming message
+%         for j = 1:length(ej)
+%             m(:,j) = ep(:,:,j)*nb(:,j);
+%         end
+%         sm = sum(m,2);
+%         sm = reshape(ep,2,[])*nb(:);     % sum of incoming message
+        [nodeBel(:,i),lnZ(i)] = softmax(nodePot(:,i)+sm);
+    end
+    L(t+1) = mean(lnZ);
+    if abs(L(t+1)-L(t)) < tol; break; end
+end
+L=L(2:t);
+
+edgeBel = zeros(size(edgePot));
+for e = 1:size(B,1)
+    ni = find(B(e,:));
+    edgeBel(:,:,e) = nodeBel(:,ni(1))*nodeBel(:,ni(2))';
+end
+
+function [ei,ni] = fgn(B,i)
+% factor graph neighbor
+% ei = neighbor edge index
+% ni = neighbor node index
+e = B(:,i);                         % edge indcies in the neighborhood
+n = B(e,:);                         % node indecis in the neighborhood
+n(:,i) = false;                     % exclude self
+
+ei = find(e);
+[~,ni] = max(n,[],2);
