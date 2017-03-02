37b2051b29ac06d2c0fd44e7c142cf2645b23387
Mo Chen <sth4nth@gmail.com>
2017-03-03 03:05:47 +0800

    working on graph

--- UGM/im2mrf.m
+++ UGM/im2mrf.m
@@ -3,6 +3,22 @@ function [B,np, ep] = im2mrf(X)
 nStates = 2;
 A = im2ug(X);
 B = ug2fg(A);
+
+[s,t,w] = find(tril(A));
+G = graph(s,t,w);
+n = numnodes(G);
+m = numedges(G);
+k = 2;
+[s,t] = findedge(G);
+
+npp = zeros(k,n);
+epp = zeros(k,k,m);
+npp(1,:) = -1-2.5*X(:);
+for e = 1:m
+    ps = 1.8 + .3*1/(1+abs(X(s(e))-X(t(e))));
+    epp(:,:,e) = [ps, 0; 0, ps];
+end
+
 [nEdges, nNodes] = size(B);
 ep = zeros(nStates,nStates,nEdges);
 np = zeros(nStates,nNodes);
@@ -17,3 +33,6 @@ for e = 1:nEdges
     ps = 1.8 + .3*1/(1+abs(X(idx(1))-X(idx(2))));
     ep(:,:,e) = [ps, 0; 0, ps];
 end
+pause
+
+
--- UGM/mrfMfAsync0.m
+++ UGM/mrfMfAsync0.m
@@ -1,1 +1,38 @@
 function [nodeBel, L] = mrfMfAsync0(G, nodePot, edgePot)
+% Mean field for MRF (only for undirected graph, not for factor graph)
+% TODO:
+%   1) EP style
+%   2) do not precompute potential value but only store weight (w,b)
+%   3) further improve numerical stability to make lnZ monotonically increase
+tol = 1e-4;
+epoch = 50;
+L = -inf(1,epoch+1);
+[nodeBel,lnZ] = softmax(nodePot,1);    % init nodeBel
+n = numnodes(G);
+m = numedges(G);
+for t = 1:epoch
+    for i = 1:n
+        j = neighbors(G,i);
+        e = findedge(G,i,j);    % edgePot in neighborhood
+        nb = nodeBel(:,j);      % nodeBel in neighborhood
+        ep = edgePot(:,:,e);
+%         msg = zeros(size(nb));    % incoming message
+%         for j = 1:length(ej)
+%             msg(:,j) = ep(:,:,j)*nb(:,j);
+%         end
+%         msgs = sum(msg,2);
+        msgs = reshape(ep,2,[])*nb(:);     % sum of incoming message
+        [nodeBel(:,i),lnZ(i)] = softmax(nodePot(:,i)+msgs);
+    end
+    L(t+1) = mean(lnZ);
+%     if abs(L(t+1)-L(t)) < tol; break; end
+end
+L=L(2:t);
+
+edgeBel = zeros(size(edgePot));
+[s,t] = findedge(G);
+for e = 1:m
+    edgeBel(:,:,e) = nodeBel(:,s(e))*nodeBel(:,t(e))';
+end
+
+
