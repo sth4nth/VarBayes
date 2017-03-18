cf3c441459823845b0f915b8efa08fe1776cb099
Mo Chen <sth4nth@gmail.com>
2017-03-18 19:37:58 +0800

    improve bethe

--- UGM/betheEnergy.m
+++ UGM/betheEnergy.m
@@ -1,11 +1,10 @@
-function lnZ = betheEnergy(A, nodeBel, edgeBel, nodePot, edgePot)
+function lnZ = betheEnergy(A, nodePot, edgePot, nodeBel, edgeBel)
 % Compute Bethe free energy
-Ex = dot(nodeBel(:),log(nodePot(:)));
-Exy = dot(edgeBel(:),log(edgePot(:)));
-
+edgePot = reshape(edgePot,[],size(edgePot,3));
+edgeBel = reshape(edgeBel,[],size(edgeBel,3));
+Ex = dot(nodeBel,log(nodePot),1);
+Exy = dot(edgeBel,log(edgePot),1);
+Hx = -dot(nodeBel,log(nodeBel),1);
+Hxy = -dot(edgeBel,log(edgeBel),1);
 d = full(sum(logical(A),1));
-nb = (d-1).*nodeBel;
-Hx = -dot(nb(:),log(nodeBel(:)));
-Hxy = -dot(edgeBel(:),log(edgeBel(:)));
-
-lnZ = (Ex+Exy)-(Hx-Hxy);
+lnZ = (sum(Ex)+sum(Exy))-(sum((d-1).*Hx)-sum(Hxy));
