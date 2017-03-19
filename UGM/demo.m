3b836c2a93deb3df4b6ad291faa520775f7f8bbd
Mo Chen <sth4nth@gmail.com>
2017-03-20 01:12:21 +0800

    update demo

--- UGM/demo.m
+++ UGM/demo.m
@@ -15,38 +15,30 @@ colormap gray
 title('Noisy X');
 %% Mean Field
 [A,np,ep] = im2mrf(X);
-% [nbmf, ebmf, L] = meanField(A, np, ep);
-% subplot(3,3,4);
-% imagesc(reshape(nbmf(2,:),nRows,nCols));
-% colormap gray
-% title('Mean Field');
+[nbmf, ebmf, L] = meanField(A, np, ep);
+subplot(3,3,3);
+imagesc(reshape(nbmf(2,:),nRows,nCols));
+colormap gray
+title('MF');
 %% Belief Propagation
-tic
 [nbbp,ebbp] = belProp(A, np, ep);
-toc
+subplot(3,3,4);
+imagesc(reshape(nbbp(2,:),nRows,nCols));
+colormap gray
+title('BP');
+%% Expectation Propagation
+[nbep,ebep] = expProp(A, np, ep);
 subplot(3,3,5);
-imagesc(reshape(nbbp(2,:),nRows,nCols));
+imagesc(reshape(nbep(2,:),nRows,nCols));
 colormap gray
-title('Belief Propagation');
-%%
-tic
-[nbep,ebep] = belProp0(A, np, ep);
-toc
+title('EP');
+%% EP-BP
+[nbebp,ebebp] = expBelProp(A, np, ep);
 subplot(3,3,6);
-imagesc(reshape(nbbp(2,:),nRows,nCols));
+imagesc(reshape(nbebp(2,:),nRows,nCols));
 colormap gray
-title('Expectation Propagation');
+title('EBP');
 
 % maxdiff(nbbp,nbep)
 % maxdiff(ebbp,ebep)
-%%
-tic
-[nbep,ebep] = belProp1(A, np, ep);
-toc
-subplot(3,3,6);
-imagesc(reshape(nbbp(2,:),nRows,nCols));
-colormap gray
-title('Expectation Propagation');
 
-maxdiff(nbbp,nbep)
-maxdiff(ebbp,ebep)
