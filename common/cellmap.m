957a4de8ecdc80d4cb6e830865c79437d5b4bfb8
Mo Chen <sth4nth@gmail.com>
2017-03-30 02:40:03 +0800

    update

--- common/cellmap.m
+++
@@ -1,5 +0,0 @@
-function r = cellmap(func, c)
-r = cell(size(c));
-for i = 1:numel(c)
-    r{i} = func(c{i});
-end
--- common/rndp.m
+++ common/rndp.m
@@ -6,10 +6,9 @@ function x = rndp(p, n)
 % Ouput:
 %   x: k x n generated samples x~Mul(p)
 % Written by Mo Chen (sth4nth@gmail.com).
-    if nargin == 1
-        n = 1;
-    end
-    r = rand(1,n);
-    p = cumsum(p(:));
-    [~,x] = histc(r,[0;p/p(end)]);
+if nargin == 1
+    n = 1;
 end
+r = rand(1,n);
+p = cumsum(p(:));
+[~,x] = histc(r,[0;p/p(end)]);
