3564de90be77aede03c2e51ae1d9619e4782c380
Mo Chen <sth4nth@gmail.com>
2017-09-12 22:50:01 +0800

    remove wrong bound in GMRF

--- GMRF/gibbsGmrf.m
+++ GMRF/gibbsGmrf.m
@@ -1,5 +1,18 @@
 function lnZ = gibbsGmrf(x, lambda, L, mu)
 % Compute Gibbs free energy of Gaussian MRF
+
+
+dg = sub2ind([n,n],1:n,1:n);
+L(dg) = 0;              % remove diagonal elements
+
+
+
+[s,t,e] = find(tril(L));
+edgeBel = zeros(size(edgePot));
+for l = 1:numel(e)
+    edgeBel(:,:,e(l)) = nodeBel(:,s(l))*nodeBel(:,t(l))';
+end
+
 edgePot = reshape(edgePot,[],size(edgePot,3));
 edgeBel = reshape(edgeBel,[],size(edgeBel,3));
 Ex = dot(nodeBel,nodePot,1);
--- GMRF/gmrfMeanField.m
+++ GMRF/gmrfMeanField.m
@@ -1,4 +1,4 @@
-function [mu, LB] = gmrfMeanField(x, lambda, L, epoch)
+function mu = gmrfMeanField(x, lambda, L, epoch)
 % Mean field for latent Gaussian MRF
 % Written by Mo Chen (sth4nth@gmail.com)
 if nargin < 4
@@ -7,18 +7,11 @@ end
 n = numel(x);
 eta = lambda.*x;
 Lambda = lambda+reshape(diag(L),size(x));  % diagonal of Lambda
-dg = sub2ind([n,n],1:n,1:n);
-L(dg) = 0;              % remove diagonal elements
+L(sub2ind([n,n],1:n,1:n)) = 0;              % remove diagonal elements
 mu = x;
-
-C = n*log(2*pi)-sum(log(Lambda(:)));
-LB = -inf(1,epoch+1);
 for iter = 1:epoch
     for i = 1:numel(mu)
         [~,j,w] = find(L(i,:));             % neighbors
         mu(i) = (eta(i)-sum(w.*mu(j)))/Lambda(i);
     end
-    T = 0.5*Lambda.*mu.^2-eta.*mu;
-    LB(iter+1) = 0.5*(C-sum(T(:))); 
 end
-LB = LB(2:end);
