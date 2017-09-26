bfc41b57815c67bcf3e07261636fd8d92b3b5108
Mo Chen <sth4nth@gmail.com>
2017-09-26 21:08:41 +0800

    update

--- GMRF/gmrfBelProp.m
+++ GMRF/gmrfBelProp.m
@@ -1,12 +1,12 @@
-function mu = gmrfBelProp(x, lambda, L, epoch)
+function mu = gmrfBelProp(Lambda, eta, epoch)
 % Belief propagation for latent Gaussian MRF
 % Written by Mo Chen (sth4nth@gmail.com)
-if nargin < 4
+if nargin < 3
     epoch = 10;
 end
 
-M = zeros(size(L));                   % message matrix
-
+J
+h
 for iter = 1:epoch
     for i = 1:numel(mu)
         [~,j,e] = find(Lij(i,:));             % neighbors
