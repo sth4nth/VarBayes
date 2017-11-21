176323acbd80db3dc0968906e8e753184f3a5730
Mo Chen <sth4nth@gmail.com>
2017-11-21 18:59:48 +0800

    update

---
+++ common/fg2ug.m
@@ -0,0 +1,9 @@
+function A = fg2ug(B)
+% convert adjacent matrix of bipartite factor graph to adjacent matrix of undirected graph
+% Input:
+%   B: m x n adjacent matrix of bipartite factor graph 
+%       where m and n are # of edges and nodes
+% Output:
+%   A: n x n adjacent matrix of undirected graph
+% Written by Mo Chen (sth4nth@gmail.com)
+A = setdiag(B'*B,0);
