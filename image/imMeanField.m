d509b9f9cde53d5c3e2ec92ce79368cd2ad217d8
Mo Chen <sth4nth@gmail.com>
2017-11-22 22:20:26 +0800

    rename

--- image/imMeanField.m
+++ image/imMeanField.m
@@ -1,18 +1,16 @@
-function mu = isingMeanField(J, h, epoch)
-if nargin < 3
-    epoch = 10;
-end
-[M,N] = size(h);
-mu =  tanh(h);
+function nodeBel = imMeanField(M, N, nodePot, edgePot, epoch)
+
 stride = [-1,1,-M,M];
+nodeBel = softmax(-nodePot,1);
 for t = 1:epoch
     for j = 1:N
         for i = 1:M
             pos = i + M*(j-1);
             ne = pos + stride;
             ne([i,i,j,j] == [1,M,1,N]) = [];
-            mu(i,j) = tanh(J*sum(mu(ne)) + h(i,j));
+            nodeBel(:,pos) = softmax(-edgePot*sum(nodeBel(:,ne),2)-nodePot(:,pos));
         end
     end
 end 
+
 
