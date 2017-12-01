9d095b5c5e3017b15138c01ecf5d6e4235281b77
Mo Chen <sth4nth@gmail.com>
2017-12-01 19:25:47 +0800

    tensor operation for discrete distribution

---
+++ example/demo.m
@@ -0,0 +1,16 @@
+A = normalize(rand(2),1);   % p(x|z)
+B = normalize(rand(2),1);   % p(y|z)
+v = normalize(rand(2,1));   % p(z)
+P2 = A*diag(v)*B';          % p(x,y)
+
+l = numel(v);
+m = size(A,1);
+n = size(B,1);
+A = reshape(A,[m,1,l]);
+B = reshape(B,[1,n,l]);
+v = reshape(v,[1,1,l]);
+P3 = A.*B.*v;               % P(x,y,z);
+P = sum(P3,3);              % p(x,y)
+maxdiff(P,P2)
+
+P = P3./P2;                 % p(z|x,y);
