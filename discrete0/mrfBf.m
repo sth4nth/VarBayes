ec39baf118569e2685fd7b59d70e20cc9ed4de64
Mo Chen <sth4nth@gmail.com>
2018-03-09 04:16:35 +0800

    exact tbd

--- discrete0/mrfBf.m
+++ discrete0/mrfBf.m
@@ -1,4 +1,4 @@
-function [nodeBel, edgeBel] = mrfBf(A, nodePot, edgePot)
+function [z, lnZ] = mrfBf(A, nodePot, edgePot)
 % Brute force method for exact MRF inference
 % Assuming egdePot is symmetric
 % Input: 
@@ -9,3 +9,33 @@ function [nodeBel, edgeBel] = mrfBf(A, nodePot, edgePot)
 %   nodeBel: k x n node belief
 %   edgeBel: k x k x m edge belief
 % Written by Mo Chen (sth4nth@gmail.com)
+[k,n] = size(nodePot);
+lnZ = -inf;
+for i = 1:k^n
+    z0 = num2code(i,k,n)+1;
+    lnZ0 = potential(z0,A,nodePot,edgePot);
+    if lnZ0 > lnZ
+        lnZ = lnZ0;
+        z = z0;
+    end
+end
+
+% TBD
+function code = num2code(x, k, n)
+code = zeros(1,n);
+p = 1;
+while x
+    code(p) = mod(x,k);
+    x = x-k;
+    p = p+1;
+end
+
+function pot = potential(z, A, nodePot,edgePot)
+pot = 0;
+for i = 1:size(nodePot,2)
+   pot = pot+nodePot(i,z(i));
+end
+[s,t,e] = find(tril(A));
+for l = 1:numel(e)
+   pot = pot+edgePot(z(s(l)),z(t(l)),e(l));
+end
